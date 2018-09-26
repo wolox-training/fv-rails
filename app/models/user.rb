527014b74255792017cdf418b58cce3d3f62cda1
FVolonnino <federico.volonnino@wolox.com.ar>
2018-09-26 14:29:43 -0300

    Changed a few things around so Rubocop stops crying

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,6 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
 
+  has_many :rents, dependent: :destroy
 
-  # attr_reader :verification_code
 end
