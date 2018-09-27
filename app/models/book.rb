f5343e3202e62a7ad8f4cb71edbd4006b6821bf8
FVolonnino <federico.volonnino@wolox.com.ar>
2018-09-27 12:33:51 -0300

    Added a few more changes for Rubocop

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,4 @@
 class Book < ApplicationRecord
-  validates :title, presence: :true
-  validates :author, presence: :true
+  validates :title, presence: true
+  validates :author, presence: true
 end
