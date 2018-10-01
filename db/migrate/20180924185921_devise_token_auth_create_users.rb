25f60f070abb38350030abf0569409334623f7af
FVolonnino <federico.volonnino@wolox.com.ar>
2018-10-01 16:09:51 -0300

    Fixed a problem with migrations

--- db/migrate/20180924185921_devise_token_auth_create_users.rb
+++ db/migrate/20180924185921_devise_token_auth_create_users.rb
@@ -1,11 +1,7 @@
 class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[5.2]
   def change
-
     add_column :users, :provider, :string, :null => false, :default => "email"
     add_column :users, :uid, :string, :null => false, :default => ""
-
     add_index :users, [:uid, :provider],     unique: true
-    add_index :users, :confirmation_token,   unique: true
-    
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -18,21 +18,16 @@ ActiveRecord::Schema.define(version: 2018_09_28_143747) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "first_name", null: false
-    t.string "last_name", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.json "tokens"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name", null: false
+    t.string "last_name", null: false
     t.string "provider", default: "email", null: false
     t.string "uid", default: "", null: false
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
