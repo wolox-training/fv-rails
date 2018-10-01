c358478e8d595cd3f8a1599d79ec068c91d8927a
FVolonnino <federico.volonnino@wolox.com.ar>
2018-10-01 17:21:12 -0300

    Updated the Book Controller test

--- spec/controllers/book_controller_spec.rb
+++ spec/controllers/book_controller_spec.rb
@@ -1,17 +1,47 @@
 require 'rails_helper'
+require 'support/shared_context'
 
-RSpec.describe BookController, type: :controller do
+describe BookController, type: :controller do
+  include_context 'Authenticated User'
+
   describe 'GET #index' do
-    it 'returns http success' do
-      get :index
-      expect(response).to have_http_status(:success)
+    context 'When fetching all the books' do
+      let!(:books) { create_list(:book, 3) }
+
+      before do
+        get :index, params: { id: user.id }
+      end
+
+      it 'responses with the books json' do
+        expected = ActiveModel::Serializer::CollectionSerializer.new(
+          rents, each_serializer: RentSerializer
+        ).to_json
+        expect(response_body.to_json) =~ JSON.parse(expected)
+      end
+
+      it 'responds with 200 status' do
+        expect(response).to have_http_status(:ok)
+      end
     end
   end
 
   describe 'GET #show' do
-    it 'returns http success' do
-      get :show
-      expect(response).to have_http_status(:success)
+    context 'When fetching a book' do
+      let!(:book) { create(:book) }
+
+      before do
+        get :show, params: { user_id: user.id, id: rent.id }
+      end
+
+      it 'responses with the book json' do
+        expect(response_body.to_json).to eq RentSerializer.new(
+          rent, root: false
+        ).to_json
+      end
+
+      it 'responds with 200 status' do
+        expect(response).to have_http_status(:ok)
+      end
     end
   end
 end
