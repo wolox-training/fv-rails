2fe66fded725fa0aa106bf6ce18e04c3570f7540
FVolonnino <federico.volonnino@wolox.com.ar>
2018-10-01 15:19:01 -0300

    Updated specs to use Factory Bot

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 describe Book, type: :model do
+
+  #  before(:all) do
+  #    @book = create(:book)
+  #  end
+
   it do
     should validate_presence_of(:title)
   end
@@ -8,9 +13,9 @@ describe Book, type: :model do
     should validate_presence_of(:author)
   end
 
-  subject(:book) do
-    create(:book)
-  end
+  #  subject(:book) do
+  #    create(:book)
+  #  end
 
   #  let(:title)                { Faker::Book.title }
   #  let(:author)               { Faker::Book.author }
@@ -19,14 +24,18 @@ describe Book, type: :model do
   #  let(:image)                { ':O' }
   #  let(:year)                 { '2018' }
 
-  it do
-    is_expected.to be_valid
-  end
 
   describe '#create' do
-    context 'When the author is nil' do
-      let(:book) { create(:book, author: '') }
 
+    context 'When the book is properly set' do
+      let(:book) { create(:book) }
+      it do
+        expect(book).to be_valid
+      end
+    end
+
+    context 'When the author is nil' do
+      let(:nilAuthor) { create(:book, author: '') }
       it do
         is_expected.to be_invalid
       end
@@ -39,5 +48,38 @@ describe Book, type: :model do
         is_expected.to be_invalid
       end
     end
+
+    context 'When the genre is nil' do
+      let(:nilGenre) { create(:book, genre: '') }
+
+      it do
+        is_expected.to be_invalid
+      end
+    end
+
+    context 'When the image is nil' do
+      let(:nilImage) { create(:book, image: '') }
+
+      it do
+        is_expected.to be_invalid
+      end
+    end
+
+    context 'When the publisher is nil' do
+      let(:nilPublisher) { create(:book, publisher: '') }
+
+      it do
+        is_expected.to be_invalid
+      end
+    end
+
+    context 'When the year is nil' do
+      let(:nilYear) { create(:book, year: '') }
+
+      it do
+        is_expected.to be_invalid
+      end
+    end
+
   end
 end
