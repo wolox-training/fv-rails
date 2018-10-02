3ea87cfc26920310623cd54e2ee7c93eeae37c40
FVolonnino <federico.volonnino@wolox.com.ar>
2018-10-02 15:57:13 -0300

    Updated user authentication and it's working well now

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
-  include DeviseTokenAuth::Concerns::User
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable
+         
+  include DeviseTokenAuth::Concerns::User
 
   validates :first_name, presence: true
   validates :last_name, presence: true
--- spec/support/shared_context.rb
+++ spec/support/shared_context.rb
@@ -1,10 +1,7 @@
 RSpec.shared_context 'Authenticated User' do
   let(:user) { create(:user) }
-  # let!(:access_data) { user.generate_access_token! }
-  # let!(:access_token) { access_data[:token] }
-  # let!(:renew_id) { access_data[:renew_id] }
 
-  # before do
-  #   request.headers['Authorization'] = access_token
-  # end
+  before do
+    request.headers.merge! resource.create_new_auth_token
+  end
 end
