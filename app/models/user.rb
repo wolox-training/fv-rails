a8a5b3f52e3782239ffd0959a6bb219e3178b8fe
FVolonnino <federico.volonnino@wolox.com.ar>
2018-10-02 16:18:42 -0300

    Updated schema

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-<<<<<<< HEAD
          :rememberable, :validatable
-=======
-         :recoverable, :rememberable, :validatable
->>>>>>> 1c05180... Fixed a few things for rubocop
 
   validates :first_name, presence: true
   validates :last_name, presence: true
