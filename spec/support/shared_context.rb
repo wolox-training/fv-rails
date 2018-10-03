566dc199dfff3863935c39ad93001ad97b1d6618
FVolonnino <federico.volonnino@wolox.com.ar>
2018-10-03 11:20:38 -0300

    Made some changes to Shared Contexts and Book Controller spec, all tests now work with user authentication

--- spec/support/shared_context.rb
+++ spec/support/shared_context.rb
@@ -2,6 +2,6 @@ RSpec.shared_context 'Authenticated User' do
   let(:user) { create(:user) }
 
   before do
-    request.headers.merge! resource.create_new_auth_token
+    request.headers.merge! user.create_new_auth_token
   end
 end
