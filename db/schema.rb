2bd2770ca4707920cb9d70c7d3c498483b0abc41
FVolonnino <federico.volonnino@wolox.com.ar>
2018-10-03 10:41:06 -0300

    Uploading updated version of schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_28_143747) do
+ActiveRecord::Schema.define(version: 2018_10_02_193228) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2018_09_28_143747) do
   create_table "books", force: :cascade do |t|
     t.string "genre", null: false
     t.string "author", null: false
-    t.string "image"
+    t.string "image", null: false
     t.string "title", null: false
     t.string "publisher", null: false
     t.string "year", null: false
@@ -32,21 +32,13 @@ ActiveRecord::Schema.define(version: 2018_09_28_143747) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-<<<<<<< HEAD
-=======
-    t.json "tokens"
->>>>>>> 344f853... Deleted redundant migration
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "first_name", null: false
     t.string "last_name", null: false
     t.string "provider", default: "email", null: false
     t.string "uid", default: "", null: false
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.json "tokens"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
