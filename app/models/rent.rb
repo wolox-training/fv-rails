c4a6829d10b42932772790a3bcb0ae418ea87dd3
FVolonnino <federico.volonnino@wolox.com.ar>
2018-10-03 15:48:01 -0300

    Included model validations for initial_date and final_date

--- app/models/rent.rb
+++ app/models/rent.rb
@@ -1,5 +1,5 @@
 class Rent < ApplicationRecord
   belongs_to :user
   belongs_to :book
-  validates :user, :book, presence: true
+  validates :user, :book, :initial_date, :final_date, presence: true
 end
