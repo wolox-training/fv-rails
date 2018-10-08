5f6fecac96b7ba1db0c9cbdb693e6d93c7d68f3e
FVolonnino <federico.volonnino@wolox.com.ar>
2018-10-08 15:44:36 -0300

    Forgot to add Rent resources :P

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   api_version(:module => "Api/V1", :path => {:value => "api/v1"}) do
     resources :book, only: [:index, :show]
+    resources :rent, only: [:index, :create]
   end
   mount_devise_token_auth_for 'User', at: 'auth'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
