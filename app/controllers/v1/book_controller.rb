a9be207b2e31d6533b5cbc6f95d23157caf0d283
FVolonnino <federico.volonnino@wolox.com.ar>
2018-10-09 12:47:17 -0300

    Added versionist to Gemfile and used it to generate v1

--- app/controllers/v1/book_controller.rb
+++ app/controllers/v1/book_controller.rb
@@ -1,13 +1,10 @@
-class BookController < ApplicationController
+class V1::BookController < V1::BaseController
   include Wor::Paginate
   include DeviseTokenAuth::Concerns::SetUserByToken
-
   def index
     render_paginated Book.all
   end
-
   def show
     render json: Book.find(params[:id])
   end
-  before_action :authenticate_user!
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-
-  resources :book, only: [:index, :show]
+  api_version(:module => "V1", :path => {:value => "v1"}) do
+    resources :book, only: [:index, :show]
+  end  
   mount_devise_token_auth_for 'User', at: 'auth'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
