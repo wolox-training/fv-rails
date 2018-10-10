5edeba9424141369f926967d2615a1b8d17d53ed
FVolonnino <federico.volonnino@wolox.com.ar>
2018-10-10 15:11:01 -0300

    Fixed validations for api/v1 controllers

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -3,7 +3,7 @@ module Api
     class ApiController < ApplicationController
       include Wor::Paginate
       include DeviseTokenAuth::Concerns::SetUserByToken
-      protect_from_forgery with: :exception
+      protect_from_forgery with: :null_session
       before_action :authenticate_user!
     end
   end
