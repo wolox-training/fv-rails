3ac29b63baa6c0bb72f5f46dab63fce97a7726be
FVolonnino <federico.volonnino@wolox.com.ar>
2018-10-10 14:45:05 -0300

    Protect from forgery now works with exception

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -3,7 +3,7 @@ module Api
     class ApiController < ApplicationController
       include Wor::Paginate
       include DeviseTokenAuth::Concerns::SetUserByToken
-      protect_from_forgery with: :null_session
+      protect_from_forgery with: :exception
       before_action :authenticate_user!
     end
   end
