f26c48bcd546ee87a23778d5dc12eb6efe70896f
FVolonnino <43348036+FVolonnino@users.noreply.github.com>
2018-10-11 12:17:51 -0300

    Create index rent (#16)
    * Index and Create paths are now working for Rents
    
    * Fixed a few issues with specs
    
    * Something went wrong but I think I fixed it
    
    * Rent index now uses a user_id param
    
    * Forgot to add Rent resources :P
    
    * Added the changes requested in the last review except for the versioning (will leave it until rebase of previous branch)
    
    * Changing a few things about controllers
    
    * Moved a few things around after the rebase
    
    * Added a RentSerializer to make it look more like the documentation

---
+++ app/controllers/api/v1/rent_controller.rb
@@ -0,0 +1,28 @@
+module Api
+  module V1
+    class RentController < ApiController
+      def index
+        if current_user
+          render_paginated Rent.where(user_id: current_user.id)
+        else
+          render json: 'Error: cannot find user'
+        end
+      end
+
+      def create
+        rent = Rent.new(create_params)
+        if rent.save
+          render json: 'Rent created and saved!'
+        else
+          render json: 'Something went wrong and your rent creation failed :('
+        end
+      end
+
+      private
+
+      def create_params
+        params.permit(:user_id, :book_id, :initial_date, :final_date)
+      end
+    end
+  end
+end
