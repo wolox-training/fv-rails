a6627ed46023eb8d24afd81b0cf958ce73ead05a
FVolonnino <federico.volonnino@wolox.com.ar>
2018-10-12 12:26:48 -0300

    Fixed the book suggestion factory

--- spec/factories/book_suggestions.rb
+++ spec/factories/book_suggestions.rb
@@ -1,12 +1,12 @@
 FactoryBot.define do
   factory :book_suggestion do
-    synopsis { "MyString" }
-    price { 1.5 }
-    author { "MyString" }
-    title { "MyString" }
-    link { "MyString" }
-    publisher { "MyString" }
-    year { "MyString" }
+    synopsis { Faker::HowIMetYourMother.quote }
+    price { Faker::Number.number(3) }
+    author { Faker::Book.author  }
+    title { Faker::Book.title }
+    link { Faker::Internet.url }
+    publisher { Faker::Book.publisher }
+    year { Faker::Time.backward(14, :evening).year }
     user { nil }
   end
 end
