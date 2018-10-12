ee9eb4728b4069175387156ff09df054070317e1
FVolonnino <federico.volonnino@wolox.com.ar>
2018-10-12 12:30:15 -0300

    These Rubocop corrections are getting ridiculous

--- spec/factories/book_suggestions.rb
+++ spec/factories/book_suggestions.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :book_suggestion do
     synopsis { Faker::HowIMetYourMother.quote }
     price { Faker::Number.number(3) }
-    author { Faker::Book.author  }
+    author { Faker::Book.author }
     title { Faker::Book.title }
     link { Faker::Internet.url }
     publisher { Faker::Book.publisher }
