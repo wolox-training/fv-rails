87c4228250273545a35e14abab34d601dc65efe0
FVolonnino <federico.volonnino@wolox.com.ar>
2018-10-12 17:30:03 -0300

    Added specs for bookSuggestion#create which tests for book suggestions created by users and guests

--- spec/controllers/api/v1/book_suggestion_controller_spec.rb
+++ spec/controllers/api/v1/book_suggestion_controller_spec.rb
@@ -3,44 +3,42 @@ describe Api::V1::BookSuggestionController, type: :controller do
   include Devise::Test::ControllerHelpers
 
   describe 'POST #create' do
-
-    context 'When creating a valid user BookSuggestion' do
+    context 'When creating a BookSuggestion with a valid user' do
       let!(:book_sugg) { create(:book_suggestion, user: user) }
+      let(:post_create) do
+        post :create,
+             params: { user_id: book_sugg.user_id, title: book_sugg.title,
+                       link: book_sugg.link, year: book_sugg.year,
+                       publisher: book_sugg.publisher, author: book_sugg.author }
+      end
       it 'creates a new book_suggestion' do
         expect do
-          post :create,
-            params: { user_id: user.id, title: book_sugg.title,
-                      link: book_sugg.link, year: book_sugg.year,
-                      publisher: book_sugg.publisher, author: book_sugg.author }
+          post_create
         end.to change { BookSuggestion.count }.by(1)
       end
 
       it 'responds with 201 status' do
-        post :create,
-          params: { user_id: user.id, title: book_sugg.title,
-                    link: book_sugg.link, year: book_sugg.year,
-                    publisher: book_sugg.publisher, author: book_sugg.author }
+        post_create
         expect(response).to have_http_status(:created)
       end
     end
 
-    context 'When creating a book suggestion with no user' do
+    context 'When creating a BookSuggestion with no user' do
       let!(:book_sugg) { create(:book_suggestion) }
-
+      let(:post_create) do
+        post :create,
+             params: { user_id: book_sugg.user_id, title: book_sugg.title,
+                       link: book_sugg.link, year: book_sugg.year,
+                       publisher: book_sugg.publisher, author: book_sugg.author }
+      end
       it 'creates a new book_suggestion' do
         expect do
-          post :create,
-            params: { user_id: nil, title: book_sugg.title,
-                      link: book_sugg.link, year: book_sugg.year,
-                      publisher: book_sugg.publisher, author: book_sugg.author }
+          post_create
         end.to change { BookSuggestion.count }.by(1)
       end
 
       it 'responds with 201 status' do
-        post :create,
-          params: { user_id: nil, title: book_sugg.title,
-                    link: book_sugg.link, year: book_sugg.year,
-                    publisher: book_sugg.publisher, author: book_sugg.author }
+        post_create
         expect(response).to have_http_status(:created)
       end
     end
